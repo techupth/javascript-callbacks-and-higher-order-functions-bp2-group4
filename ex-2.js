de6c899935256d72ab7191df6d4fe7bb897e2832
Suwinai <suwinai.apionline@gmail.com>
2023-12-09 19:41:26 +0700

    feat: Callback and High order function are finished

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,13 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for(let i = 0; i < array.length; i++){
+    if(operation(array[i])){
+      count ++
+    }
+  }
+  return count <= 5;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +17,8 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,(score) => score > 70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,(score) => score > 70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,(score) => score > 70);
+
+console.log(scoreRoom1Result,scoreRoom2Result,scoreRoom3Result);
